6f24995307102839532a24cb08c1d71ca5472554
Pascal K <pascal.kosak@gmail.com>
2019-08-17 15:04:19 +0200

    Fix exports

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+export * from './bot.types';
+export * from './bot.module';
+export * from './bot.options';
+export * from './bot.constants';
+export * from './bot.decorators';
+export * from './bot.interfaces';
+export * from './bot.command.service';
